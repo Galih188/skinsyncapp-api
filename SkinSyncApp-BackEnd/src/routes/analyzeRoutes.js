ea2d14ab9361e92c149a63973155ea2e4807f78c
Galihhh <127461042+Galih188@users.noreply.github.com>
2025-06-13 03:32:47 +0700

    Delete SkinSyncApp-BackEnd directory

--- SkinSyncApp-BackEnd/src/routes/analyzeRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { analyzeSkin } = require("../controllers/analyzeController");
-const router = express.Router();
-
-router.post("/analyze", analyzeSkin);
-
-module.exports = router;
